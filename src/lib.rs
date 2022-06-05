f29391ddff4d2a38a7658e3bc1769b9ca2438a11
dagadam <dagadamsven@gmail.com>
2022-06-05 13:56:49 +0200

    Parse contents to Cells instead of Options

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,58 @@
+use std::iter::Map;
+
+
+#[derive(Debug, PartialEq)]
+enum Cell {
+    Num(u32),
+    Empty(Vec<bool>), //Which values are possible
+}
+
 fn run(contents: &str) -> String {
     let parsed = parse_contents(contents);
     
     solve(parsed)
 }
 
-fn parse_contents(contents: &str) -> Vec<Vec<Option<u32>>> {
+fn parse_contents(contents: &str) -> Vec<Vec<Cell>> {
     const RADIX: u32 = 10;
 
-    let lines = contents.lines();
-    let outer_vec: Vec<Vec<Option<u32>>> = lines.map(|line| { line.chars().map(|character| { character.to_digit(RADIX) }).collect() }).collect();
+    let mut vec: Vec<Vec<Cell>> = vec![];
 
-    outer_vec
+    for line in contents.lines() {
+
+        vec.push(line.chars().map(|x| {
+
+            match x.to_digit(RADIX) {
+                Some(num) => Cell::Num(num),
+                None => Cell::Empty(vec![]),
+            }
+
+        }).collect())
+
+    };
+
+    vec
+    
+
+    // //This whole section is pretty unreadable but could be remade later (maybe with loops) if it causes problems
+    // //Find largest number
+    // let primary: Map<Vec<Option<u32>>> = lines.map(|line| 
+    //     { line.chars().map(|character| character.to_digit(RADIX)).collect() });
+
+
+
+    // let outer_vec: Vec<Vec<Cell>> = primary.map(|number: Option<u32>|
+    // { 
+    //     match number {
+    //         Some(num) => Cell::Num(num),
+    //         None => Cell::Empty(vec![true; ]),
+    //     } 
+    // }).collect();
+
+    // outer_vec
 }
 
-fn solve(sudoku: Vec<Vec<Option<u32>>>) -> String {
+fn solve(sudoku: Vec<Vec<Cell>>) -> String {
     todo!();
 }
 
@@ -55,13 +94,14 @@ _74_2_59_
 
     #[test]
     fn test_parsing() {
+        use Cell::*;
         let contents = "\
 __75__6_3
 43___6__5";
 
         let expected_result = vec![
-            vec![None,None,Some(7),Some(5),None,None,Some(6),None,Some(3)],
-            vec![Some(4),Some(3),None,None,None,Some(6),None,None,Some(5)],
+            vec![Empty(vec![]),Empty(vec![]),Num(7),Num(5),Empty(vec![]),Empty(vec![]),Num(6),Empty(vec![]),Num(3)],
+            vec![Num(4),Num(3),Empty(vec![]),Empty(vec![]),Empty(vec![]),Num(6),Empty(vec![]),Empty(vec![]),Num(5)],
         ];
 
         let result = parse_contents(contents);
